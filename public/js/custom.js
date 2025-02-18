4dcccffaf32672fec15c1e36ac9b8cb5cf2bbc84
hanmaster <w54661c@gmail.com>
2025-02-18 11:49:31 +1000

    first commit

---
+++ public/js/custom.js
@@ -0,0 +1,34 @@
+var $window = $(window);
+
+// stellar
+$window.stellar();
+
+$('#shadow-btn-1').on('click', function () {
+    $('#shadow-text-1').toggleClass('shadow-toggle');
+    $('#shadow-btn-1').toggleClass('btn-toggle');
+});
+
+$('#shadow-btn-2').on('click', function () {
+    $('#shadow-text-2').toggleClass('shadow-toggle');
+    $('#shadow-btn-2').toggleClass('btn-toggle');
+});
+
+$('#shadow-btn-3').on('click', function () {
+    $('#shadow-text-3').toggleClass('shadow-toggle');
+    $('#shadow-btn-3').toggleClass('btn-toggle');
+});
+
+$('#shadow-btn-4').on('click', function () {
+    $('#shadow-text-4').toggleClass('shadow-toggle');
+    $('#shadow-btn-4').toggleClass('btn-toggle');
+});
+
+$('#shadow-btn-5').on('click', function () {
+    $('#shadow-text-5').toggleClass('shadow-toggle');
+    $('#shadow-btn-5').toggleClass('btn-toggle');
+});
+
+$('#shadow-btn-6').on('click', function () {
+    $('#shadow-text-6').toggleClass('shadow-toggle');
+    $('#shadow-btn-6').toggleClass('btn-toggle');
+});
